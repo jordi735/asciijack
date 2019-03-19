b230966820bd3b5c4e93adb29b747bc379610612
Jordi735 <jordidorren@gmail.com>
2019-03-19 17:59:16 +0100

    ints to variables

--- src/card_factory.cpp
+++ src/card_factory.cpp
@@ -5,18 +5,22 @@
 
 std::vector<Card *> CardFactory::createSuit(CardEnum type) {
 
+    constexpr int suitSpace = 36;
+    constexpr int cardWidth = 12;
+    constexpr int cardHeight = 10;
+
     std::vector<Card *> cards(4);
 
     int currentChar, i = 0;
     while (type != static_cast<CardEnum>(i++));
-    currentChar = ((i - 1) * 36) * 12;
+    currentChar = ((i - 1) * suitSpace) * cardWidth;
     unsigned char *startAddress = (ascii_art + currentChar);
 
     for (int i = 0; i < 4; i++) {
         std::vector<std::string> art;
-        for (int j = 0; j < 9; j++)  {
+        for (int j = 0; j < cardHeight - 1; j++)  {
             std::string line;
-            for (int k = 0; k < 11; k++) {
+            for (int k = 0; k < cardWidth - 1; k++) {
                 line.push_back(*startAddress);
                 ++startAddress;
             }
